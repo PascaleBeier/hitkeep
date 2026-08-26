10045411bb2e60840369129f7016f66c7c850b85
Pascale Beier <mail@pascalebeier.de>
2026-08-27 01:28:58 +0200

    test(config): classify catalog publication surfaces

--- config/catalog.go
+++ config/catalog.go
@@ -15,18 +15,19 @@ type ConfigurationCategory struct {
 }
 
 type ConfigurationSetting struct {
-	Field           string   `json:"field"`
-	Category        string   `json:"category"`
-	ConfigFileKey   string   `json:"config_file_key"`
-	Environment     string   `json:"environment,omitempty"`
-	Flag            string   `json:"flag"`
-	DeprecatedFlags []string `json:"deprecated_flags,omitempty"`
-	Type            string   `json:"type"`
-	Default         string   `json:"default"`
-	DisplayDefault  string   `json:"display_default,omitempty"`
-	Description     string   `json:"description"`
-	Sensitive       string   `json:"sensitive,omitempty"`
-	CloudOnly       bool     `json:"cloud_only,omitempty"`
+	Field           string                        `json:"field"`
+	Category        string                        `json:"category"`
+	ConfigFileKey   string                        `json:"config_file_key"`
+	Environment     string                        `json:"environment,omitempty"`
+	Flag            string                        `json:"flag"`
+	DeprecatedFlags []string                      `json:"deprecated_flags,omitempty"`
+	Type            string                        `json:"type"`
+	Default         string                        `json:"default"`
+	DisplayDefault  string                        `json:"display_default,omitempty"`
+	Description     string                        `json:"description"`
+	Sensitive       string                        `json:"sensitive,omitempty"`
+	CloudOnly       bool                          `json:"cloud_only,omitempty"`
+	Publication     ConfigurationPublicationClass `json:"publication"`
 }
 
 type ConfigurationCatalog struct {
@@ -65,6 +66,7 @@ func Catalog() ConfigurationCatalog {
 			Description:    field.Tag.Get("desc"),
 			Sensitive:      field.Tag.Get("sensitive"),
 			CloudOnly:      field.Tag.Get("cloud") == "true",
+			Publication:    configurationPublication(field),
 		}
 		if deprecated := field.Tag.Get("deprecated"); deprecated != "" {
 			setting.DeprecatedFlags = []string{deprecated}
@@ -149,6 +151,33 @@ func PublicationRequirements() []ConfigurationPublication {
 			ConfigurationPublicationCanonicalExample: canonicalDefault,
 		},
 	}}
+}
+
+// ConfigurationPublicationClass declares how an active setting reaches an
+// operator. Every catalog setting must choose one explicitly.
+type ConfigurationPublicationClass string
+
+const (
+	ConfigurationPublicationUnclassified ConfigurationPublicationClass = ""
+	ConfigurationPublicationOperator     ConfigurationPublicationClass = "operator"
+	ConfigurationPublicationPersistent   ConfigurationPublicationClass = "persistent-data"
+	ConfigurationPublicationCommand      ConfigurationPublicationClass = "command"
+	ConfigurationPublicationCloud        ConfigurationPublicationClass = "managed-cloud"
+)
+
+func configurationPublication(field reflect.StructField) ConfigurationPublicationClass {
+	switch field.Name {
+	case "DataPath":
+		return ConfigurationPublicationPersistent
+	case "Healthcheck":
+		return ConfigurationPublicationCommand
+	case "CloudHosted", "CloudSignupEnabled", "CloudJurisdiction", "CloudRegion", "CloudUpgradeURL", "CloudSupportURL", "CloudPlanCode", "CloudPlanName", "CloudMaxTeams", "CloudMaxSitesPerTeam", "CloudMaxRetentionDays", "CloudMaxTeamMembers", "CloudAllowSSO", "CloudAllowCustomBranding", "StripeSecretKey", "StripePublishableKey", "StripeWebhookSecret", "StripePortalConfigurationID", "StripePriceProMonthly", "StripePriceBusinessMonthly", "StripePriceProAnnual", "StripePriceBusinessAnnual", "CloudCheckoutSuccessURL", "CloudCheckoutCancelURL":
+		return ConfigurationPublicationCloud
+	case "HTTPAddr", "DBPath", "BindAddr", "JoinAddr", "IngestRateLimit", "ApiRateLimit", "AuthRateLimit", "WebhookRateLimit", "DataRetentionDays", "NodeName", "DuckDBMemoryLimit", "DuckDBThreads", "DBCompactOnStart", "DBAutoRecover", "DBAutoRecoverWAL", "DBCheckpointIntervalMinutes", "DBRecoveryPath", "ArchivePath", "PublicURL", "LogLevel", "JWTSecret", "TrustedProxies", "NSQTCPAddress", "NSQHTTPAddress", "ApiBurst", "AuthBurst", "IngestBurst", "WebhookBurst", "AuthRememberMeDays", "AuthSessionMinutes", "AuthSessionWarningSeconds", "SocialGoogleClientID", "SocialGoogleClientSecret", "SocialGitHubClientID", "SocialGitHubClientSecret", "SocialMicrosoftClientID", "SocialMicrosoftClientSecret", "SocialMicrosoftTenant", "SocialSignupEnabled", "MailDriver", "MailEncryption", "MailInsecureSkipVerify", "MailHost", "MailPort", "MailUsername", "MailPassword", "MailFromAddress", "MailFromName", "SpamFilterAutoUpdate", "SpamFilterPath", "SpamFilterUpdateIntervalMin", "ImportMaxStageBytes", "ImportStageRetentionDays", "WebhookAllowDevelopmentTargets", "WebhookDeliveryTimeoutSeconds", "WebhookDeliveryConcurrency", "WebhookPerEndpointConcurrency", "WebhookMaxAttempts", "WebhookRetryBaseSeconds", "WebhookRetryMaxSeconds", "WebhookRetentionDays", "WebhookSweepSeconds", "GoogleSearchConsoleClientID", "GoogleSearchConsoleClientSecret", "GoogleSearchConsoleRedirectURL", "BackupPath", "BackupIntervalMinutes", "BackupRetentionCount", "S3AccessKeyID", "S3SecretAccessKey", "S3SessionToken", "S3Region", "S3Endpoint", "S3URLStyle", "S3UseSSL", "MCPEnabled", "MCPPath", "MCPMaxRangeDays", "MCPDocsEnabled", "MCPDocsURL", "MCPDocsCacheMinutes", "CustomTrackingDNSTarget", "CustomTrackingTLSMode", "CaddyTLSAskToken", "AIEnabled", "AskAIEnabled", "AIProvider", "AIModel", "AIBaseURL", "AIRegion", "AIAPIKey", "AITimeoutSeconds", "AIRequestLimit", "AITokenLimit", "AIBudgetWindowMinutes":
+		return ConfigurationPublicationOperator
+	default:
+		return ConfigurationPublicationUnclassified
+	}
 }
 
 func configFileKey(environment, flag string) string {
